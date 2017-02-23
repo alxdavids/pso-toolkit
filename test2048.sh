1ca62c2b9cd298b58b536e86878fcfa13275d17c
alxdavids <alex.davidson92@gmail.com>
2017-02-23 16:14:30 +0000

    update test flags

--- test2048.sh
+++ test2048.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$GOEXE test -timeout 900m -args -k 2048 -n 256
-$GOEXE test -timeout 900m -args -k 2048 -n 1024
-$GOEXE test -timeout 900m -args -k 2048 -n 4096
-$GOEXE test -timeout 900m -args -k 2048 -n 16384
-$GOEXE test -timeout 900m -args -k 2048 -n 65536
-$GOEXE test -timeout 900m -args -k 2048 -n 262144
+$GOEXE test -timeout 900m -args -k 2048 -m 8 -n 256
+$GOEXE test -timeout 900m -args -k 2048 -m 8 -n 1024
+$GOEXE test -timeout 900m -args -k 2048 -m 8 -n 4096
+$GOEXE test -timeout 900m -args -k 2048 -m 8 -n 16384
+$GOEXE test -timeout 900m -args -k 2048 -m 8 -n 65536
+$GOEXE test -timeout 900m -args -k 2048 -m 8 -n 262144
