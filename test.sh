0fedd205b6af469c5f48d36ec206231d1fd0a3f5
alxdavids <alex.davidson92@gmail.com>
2017-02-23 16:19:07 +0000

    more test params

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-$GOEXE test -timeout 900m -args -k 1024 -m 8 -n 256
-$GOEXE test -timeout 900m -args -k 1024 -m 8 -n 1024
-$GOEXE test -timeout 900m -args -k 1024 -m 8 -n 4096
-$GOEXE test -timeout 900m -args -k 1024 -m 8 -n 16384
-$GOEXE test -timeout 900m -args -k 1024 -m 8 -n 65536
-$GOEXE test -timeout 900m -args -k 1024 -m 8 -n 262144
+$GOEXE test -timeout 900m -args -k 1024 -m 16 -n 256
+$GOEXE test -timeout 900m -args -k 1024 -m 16 -n 1024
+$GOEXE test -timeout 900m -args -k 1024 -m 16 -n 4096
+$GOEXE test -timeout 900m -args -k 1024 -m 16 -n 16384
+$GOEXE test -timeout 900m -args -k 1024 -m 16 -n 65536
+$GOEXE test -timeout 900m -args -k 1024 -m 16 -n 262144
